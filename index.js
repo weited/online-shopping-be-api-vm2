0b68dee8bd8d9b235814ce2411c8b5bf1b87262e
Weite Dai <weitedai.dev@gmail.com>
2022-05-18 00:53:15 +1000

    vm2 router

--- index.js
+++ index.js
@@ -7,19 +7,18 @@ import router from './router.js';
 const app = express();
 
 const PORT = process.env.PORT || 5002;
-
+// parses JSON
 app.use(express.json());
 app.use(morgan('dev'));
+// Enable Cross-Origin Resource Sharing
 app.use(cors());
+
 app.use('/vm2/api', router);
-app.get('/vm2/api', (req, res) => {
-  res.json({ hello: 'World' });
-  console.log('Server Sent A Hello World!');
+
+app.use('*', (req, res) => {
+  res.status(404).send('<h1>No resourse found!</h1>');
 });
-app.post('/', (req, res) => {
-  res.json({ hello: 'World, post' });
-  console.log('Server Sent A Hello World!');
-});
+
 app.listen(PORT, () => {
   console.log(`app running on port ${PORT}`);
 });
--- router.js
+++ router.js
@@ -4,19 +4,22 @@ import db from './config/db.js';
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.json({ hello: 'this is ' });
-  console.log('Server Sent A Hello World!');
+  res
+    .status(200)
+    .json({ message: 'Hi, this is REST api VM2 for kit514 assignment2' });
 });
+
+/**
+ *@route   POST ip/vm2/api/cards
+ *@desc    Check card number and pin
+ */
 router.post('/cards', async (req, res) => {
   const { cardNum, cardPin } = req.body;
   try {
-    const sql = `SELECT * FROM card WHERE card_num = ?;`;
-    const [[result]] = await db.execute(sql, [cardNum]);
+    const sql = `SELECT * FROM card WHERE card_num = ? AND pin = ?;`;
+    const [[result]] = await db.execute(sql, [cardNum, cardPin]);
     if (!result) {
-      return res.status(404).json({ message: 'no card number', result });
-    }
-    if (result.pin !== cardPin) {
-      return res.status(401).json(false);
+      return res.status(403).json(false);
     }
     res.status(200).json(true);
   } catch (error) {
@@ -24,9 +27,13 @@ router.post('/cards', async (req, res) => {
     res.status(500).json(error);
   }
 });
+
+/**
+ *@route   GET ip/vm2/api/items
+ *@desc    get all items in VM2
+ */
 router.get('/items', async (req, res) => {
   const { name } = req.query;
-
   try {
     // if there is name parameter in query
     if (name) {
@@ -36,18 +43,12 @@ router.get('/items', async (req, res) => {
     }
 
     // if there isn't name parameter in query, find all items
-
     const sql = 'SELECT * FROM item;';
     const [items] = await db.execute(sql);
-    // find the items from VM2
-
-    // Combine two item lists
     return res.status(200).json({ items });
   } catch (error) {
     return res.status(500).json(error);
   }
 });
-// router.use('/items');
-// router.use('/purchases');
 
 export default router;
